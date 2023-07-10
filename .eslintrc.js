804d8943e1f764c3e842589629d59d260495f9c0
Porostik <porostik2@yandex.ru>
2023-07-10 10:36:56 +0300

    Поправил `eslint` конфиг

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,37 @@
 module.exports = {
   env: {
-    browser: true,
     es2021: true,
+    node: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ['airbnb-base'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: [
-    '@typescript-eslint',
-  ],
+  plugins: ['@typescript-eslint'],
   rules: {
+    'import/prefer-default-export': ['off'],
+    'implicit-arrow-linebreak': 0,
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        '': 'never',
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+  },
+  settings: {
+    'import/extensions': ['.ts', '.js'],
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.ts'],
+      },
+      typescript: {},
+    },
   },
 };
